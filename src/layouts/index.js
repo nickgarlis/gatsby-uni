2d6c161326911c31b1ba45dab789191132762d49
nickgarlis <nickgarlis@gmail.com>
2018-09-08 22:24:01 +0300

    Only use image for homepage

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
+import {Container, Header, Image, Segment} from 'semantic-ui-react'
 
 import NavBar from '../components/Navigation/NavBar'
-import Header from '../components/Header'
-import Main from '../components/Main'
 import Footer from '../components/Footer'
 
 import config from '../../data/SiteConfig'
@@ -32,6 +31,12 @@ class Layout extends React.Component {
     }
   }
 
+  getTitle () {
+    const {currentPath} = this.state
+    let title = currentPath.replace(/\//g, '')
+    return title[0].toUpperCase() + title.slice(1)
+  }
+
   render () {
     const {children} = this.props
     const {currentPath} = this.state
@@ -50,12 +55,37 @@ class Layout extends React.Component {
           items={config.siteNav}
           logo={config.siteLogo}
         >
-          <Header />
-          <Main>
+          {(currentPath === '/')
+            ? <Image
+              src='http://www.landezine.com/wp-content/uploads/2011/02/4-gh3_landscape-architecture-trinity-college-park.png'
+              fluid
+            />
+            : <Segment
+              inverted
+              textAlign='left'
+              style={{ minHeight: 300, padding: '1em 0em' }}
+              vertical
+            >
+              <Container>
+                <Header
+                  as='h1'
+                  content={this.getTitle()}
+                  inverted
+                  style={{
+                    fontSize: '4em',
+                    fontWeight: 'normal',
+                    marginBottom: 0,
+                    marginTop: '3em'
+                  }}
+                />
+              </Container>
+            </Segment>
+          }
+          <Container style={{ marginTop: '7em' }}>
             {children()}
-          </Main>
+          </Container>
+          <Footer logo={config.siteLogo} />
         </NavBar>
-        <Footer logo={config.siteLogo} />
       </div>
     )
   }
