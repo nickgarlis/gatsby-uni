303c6f651c83f38c78d9dc5bc5d4620c2d0233ef
nickgarlis <nickgarlis@gmail.com>
2018-09-12 19:47:27 +0300

    Add collection field for MarkdownRemark

---
+++ gatsby-node.js
@@ -0,0 +1,15 @@
+exports.onCreateNode = ({boundActionCreators, getNode, node}) => {
+  const {createNodeField} = boundActionCreators
+
+  if (node.internal.type === `MarkdownRemark`) {
+    const parent = getNode(node.parent)
+
+    if (parent.internal.type === 'File') {
+      createNodeField({
+        name: `collection`,
+        node,
+        value: parent.sourceInstanceName
+      })
+    }
+  }
+}
