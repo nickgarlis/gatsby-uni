9d8725fd0be71b6bc71dacaf8c21c8159f21a930
nickgarlis <nickgarlis@gmail.com>
2018-09-24 20:16:51 +0300

    :wrench: courses imports

--- src/pages/courses.js
+++ src/pages/courses.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Table from '../components/CoursesTable';
-import {coursesDir} from '../../data/SiteConfig';
 
 const Courses = ({data, history, location}) => {
   const courses = data.allMarkdownRemark.edges;
