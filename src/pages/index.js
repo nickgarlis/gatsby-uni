16edf4d662efd1784de0f55dbc296f01605105e2
nickgarlis <nickgarlis@gmail.com>
2018-10-04 09:45:36 +0300

    Add events to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,46 @@
 import React from 'react';
+import {Header} from 'semantic-ui-react';
+import Events from '../components/Events';
 
-const IndexPage = () => (
-  <div>
-    <h1>Welcome to Gatsby Uni</h1>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
-      adipisci, voluptatum, nisi velit praesentium error quia explicabo voluptas
-      ad recusandae enim. Recusandae sequi non vel voluptas. Labore omnis
-      provident ex, perferendis esse, voluptate atque alias officiis dolor
-      eligendi itaque qui?
-    </p>
-  </div>
-);
+const IndexPage = ({data}) => {
+  const events = data.allMarkdownRemark.edges;
+  return (
+    <div>
+      <Header as="h1">
+        Welcome to Gatsby Uni
+        <Header.Subheader>
+          A flexible Gatsby theme meant to be used by universities.
+        </Header.Subheader>
+      </Header>
+      <Events events={events} />
+    </div>
+  );
+};
 
 export default IndexPage;
+
+// TODO: Move events to the end of the list when they are over
+export const eventsQuery = graphql`
+  query EventsIndexQuery {
+    allMarkdownRemark(
+      limit: 4
+      filter: {
+        fields: {collection: {eq: "events"}}
+        frontmatter: {date: {ne: null}}
+      }
+      sort: {fields: [frontmatter___date], order: ASC}
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "dddd DD MMMM YYYY")
+            description
+            image
+          }
+        }
+      }
+    }
+  }
+`;
